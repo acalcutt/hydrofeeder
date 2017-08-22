6e035a6121f18c28b9bc977a7c9d05ee509f02b3
acalcutt <acalcutt@techidiots.net>
2017-08-21 22:53:33 -0400

    test service
    very incomplete

--- opt/hydrofeeder/hydrofeeder-service.py
+++ opt/hydrofeeder/hydrofeeder-service.py
@@ -57,6 +57,92 @@ sys.stderr = MyLogger(logger, logging.ERROR)
 GPIO.setmode(GPIO.BCM)
 con = lite.connect('/opt/hydrofeeder/hydrofeeder.sqlite')
 
+
+
+
+#Initiate PUMP GPIO
+with con:
+    
+    cur = con.cursor()    
+    cur.execute("SELECT id, name, gpio, ml_per_minute, on_value, off_value FROM pump")
+
+    while True:
+        row = cur.fetchone()
+        
+        if row == None:
+            break
+
+        print 'Initiating Pump', row[0], row[1], row[2], row[3], row[4], row[5]
+
+        GPIO.setup(row[2], GPIO.OUT)
+        if row[5] == 1:
+           GPIO.output(row[2], GPIO.HIGH)
+        else:
+           GPIO.output(row[2], GPIO.LOW)
+while True:
+    #Get Manual Pump Jobs
+    with con:
+        
+        cur = con.cursor()    
+        cur.execute("SELECT id, grow_id, pump_id, add_ml FROM grow_pump_manual WHERE completed != 1")
+        while True:
+            row = cur.fetchone()
+            
+            if row == None:
+                break
+
+            print 'Manual Pump Job:', row[0], row[1], row[2], row[3]
+            job_id = row[0]
+            pump_id = row[2]
+            add_ml = row[3]
+
+            cur2 = con.cursor()
+            cur2.execute('UPDATE grow_pump_manual SET in_progress = 1')
+            
+            cur3 = con.cursor()
+            cur3.execute('SELECT id, name, gpio, ml_per_minute, on_value, off_value FROM pump WHERE id=?', (pump_id,))
+            pump_values = cur3.fetchone()
+            pump_gpio = pump_values[2]
+            pump_name = pump_values[1]
+            ml_per_minute = pump_values[3]
+            on_value = pump_values[4]
+            off_value = pump_values[5]
+            
+            fill_seconds = (float(add_ml) / float(ml_per_minute)) * 60
+            print fill_seconds, add_ml, ml_per_minute, (float(add_ml) / float(ml_per_minute))
+            
+            print 'Pumping', pump_name, 'for', fill_seconds, 'seconds'
+            if on_value == 1:
+               GPIO.output(pump_gpio, GPIO.HIGH)
+            else:
+               GPIO.output(pump_gpio, GPIO.LOW)
+               
+            cur4 = con.cursor()
+            cur4.execute('UPDATE grow_pump_manual SET in_progress = 0, completed = 1')
+            
+            time.sleep(fill_seconds); 
+            
+            if off_value == 1:
+               GPIO.output(pump_gpio, GPIO.HIGH)
+            else:
+               GPIO.output(pump_gpio, GPIO.LOW)
+
+            time.sleep(1)
+    time.sleep(5)
+GPIO.cleanup()
+
+
+
+
+
+
+
+
+
+
+
+
+
 #Initiate PUMP GPIO
 with con:
     
